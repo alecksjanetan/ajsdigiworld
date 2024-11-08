432a8c6abddb97b4eece34d8743810c0ca0a36ae
Alex Jane Tan <87772833+alecksjanetan@users.noreply.github.com>
2024-11-08 23:35:26 +0800

    contact form

---
+++ send_email.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data and sanitize it
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    // Email configuration
+    $to = "alexjanetan@gmail.com"; // Replace with your email address
+    $subject = "New Contact Form Submission from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Construct the email content
+    $emailBody = "You have received a new message from the contact form:\n\n";
+    $emailBody .= "Name: $name\n";
+    $emailBody .= "Email: $email\n\n";
+    $emailBody .= "Message:\n$message\n";
+
+    // Send the email
+    if (mail($to, $subject, $emailBody, $headers)) {
+        echo "<h3>Thank you! Your message has been sent.</h3>";
+    } else {
+        echo "<h3>Sorry, there was an issue sending your message. Please try again later.</h3>";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
