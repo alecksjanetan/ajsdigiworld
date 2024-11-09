99413daf4b9ce59653dba8acd5d5f20920f06890
Alex Jane Tan <87772833+alecksjanetan@users.noreply.github.com>
2024-11-10 02:54:41 +0800

    changes

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     const navMenu = document.querySelector('.nav-menu');
     navMenu.classList.toggle('active');
 }
+
+document.addEventListener('click', function(event) {
+    const navMenu = document.querySelector('.nav-menu');
+    const menuIcon = document.querySelector('.menu-icon');
+    if (navMenu.classList.contains('active') && !navMenu.contains(event.target) && !menuIcon.contains(event.target)) {
+        navMenu.classList.remove('active');
+    }
+});
