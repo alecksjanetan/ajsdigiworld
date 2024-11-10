aee1529b8b3f3fbd29444fe9211a4b9d850c6520
Alex Jane Tan <87772833+alecksjanetan@users.noreply.github.com>
2024-11-10 16:33:56 +0800

    services collapsible

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ document.addEventListener('click', function(event) {
         navMenu.classList.remove('active');
     }
 });
+
+document.querySelectorAll(".collapsible-toggle").forEach(button => {
+  button.addEventListener("click", () => {
+    const content = button.nextElementSibling;
+
+    // Toggle the display of the content
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+});
