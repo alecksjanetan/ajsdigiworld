b0d6e6083507211d3b701def9042b45b221cff51
Alex Jane Tan <87772833+alecksjanetan@users.noreply.github.com>
2024-11-10 17:35:02 +0800

    from

--- send_email.php
+++ send_email.php
@@ -1,28 +1,28 @@
+
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data and sanitize it
-    $name = htmlspecialchars($_POST["name"]);
-    $email = htmlspecialchars($_POST["email"]);
-    $message = htmlspecialchars($_POST["message"]);
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
-    // Email configuration
-    $to = "contact-me@ajsdigiworld.com"; // Replace with your email address
-    $subject = "New Contact Form Submission from $name";
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    // Email information
+    $to = "contact-me@ajsdigiworld.com";
+    $subject = "New message from $name";
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "Content-Type: text/plain; charset=UTF-8";
 
-    // Construct the email content
-    $emailBody = "You have received a new message from the contact form:\n\n";
-    $emailBody .= "Name: $name\n";
-    $emailBody .= "Email: $email\n\n";
-    $emailBody .= "Message:\n$message\n";
+    // The email content
+    $body = "You have received a new message from $name.\n\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
 
-    // Send the email
-    if (mail($to, $subject, $emailBody, $headers)) {
-        echo "<h3 style=font-family: 'Lexique'; font-size: 30px; color: #f0737f;>Thank you! Your message has been sent.</h3>";
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
     } else {
-        echo "<h3>Sorry, there was an issue sending your message. Please try again later.</h3>";
+        echo "There was an error sending your message.";
     }
 } else {
     echo "Invalid request.";
