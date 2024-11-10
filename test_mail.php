f978b879638c4708c5739d2b66346928c167d65b
Alex Jane Tan <87772833+alecksjanetan@users.noreply.github.com>
2024-11-10 17:59:45 +0800

    test

--- test_mail.php
+++ test_mail.php
@@ -1,5 +1,5 @@
 <?php
-if (mail('alexjanetan@gmail.com', 'Mail Test', 'This is a test email from PHP mail() function.')) {
+if (mail('ajsdigiworld.com', 'Mail Test', 'This is a test email from PHP mail() function.')) {
     echo 'Mail sent successfully.';
 } else {
     echo 'Mail failed to send.';
