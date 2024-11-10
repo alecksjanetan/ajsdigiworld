db08415f0c1905ec4c0e5bba654f190601a1b19c
Alex Jane Tan <87772833+alecksjanetan@users.noreply.github.com>
2024-11-11 00:32:30 +0800

    php changes

--- send_email.php
+++ send_email.php
@@ -7,9 +7,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '/public_html/PHPMailer-master/src/Exception.php';
-require '/public_html/PHPMailer-master/src/PHPMailer.php';
-require '/public_html/PHPMailer-master/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
 
 // Create a new instance of PHPMailer
 $mail = new PHPMailer(true);
